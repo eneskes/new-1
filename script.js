9289fd67917cba843dc8b527df45c3f9acadf4d7
Enes Keskin <eneskeskin11@gmail.com>
2022-06-29 22:12:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var x=10;
+var y=20;
+var z=10;
+
+console.log(x+y);
+console.log(x-y);
+console.log(y/x);
+console.log(x*y);
+console.log(x*z+y);
+console.log(x*(y+z));
+
+var sonuc = x>y;
+console.log(sonuc);
